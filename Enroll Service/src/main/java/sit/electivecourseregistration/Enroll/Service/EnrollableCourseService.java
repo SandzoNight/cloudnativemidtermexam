b7a018a65c9d45e0ffb6539f5772c4e56e4bf58c
SandzoNight <flukemdc@gmail.com>
2018-10-21 18:19:08 +0700

    [UPDATE] Can now create enrollable course

--- Enroll Service/src/main/java/sit/electivecourseregistration/Enroll/Service/EnrollableCourseService.java
+++ Enroll Service/src/main/java/sit/electivecourseregistration/Enroll/Service/EnrollableCourseService.java
@@ -11,11 +11,23 @@ public class EnrollableCourseService {
     @Autowired
     private EnrollableCourseRepository enrollableCourseRepository;
 
+    @Autowired
+    private CourseService courseService;
+
     public List<EnrollableCourse> getAllEnrollableCourses() {
         return enrollableCourseRepository.findAll();
     }
 
     public EnrollableCourse getEnrollableCourseById(Long id) {
         return enrollableCourseRepository.getOne(id);
+    }
+
+    public EnrollableCourse createEnrollableCourse(Long courseId, int maxSeat, List<Integer> allowedYears) {
+        Course course = courseService.getCourseById(courseId);
+        EnrollableCourse enrollableCourse = new EnrollableCourse();
+        enrollableCourse.setCourse(course);
+        enrollableCourse.setMaxSeat(maxSeat);
+        enrollableCourse.setAllowedYear(allowedYears);
+        return enrollableCourseRepository.save(enrollableCourse);
     }
 }
