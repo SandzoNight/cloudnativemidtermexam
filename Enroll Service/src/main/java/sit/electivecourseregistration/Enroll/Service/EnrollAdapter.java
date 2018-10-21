6f11a65ad9e6d6681dc5b5d3b6c08178ec87245b
SandzoNight <flukemdc@gmail.com>
2018-10-21 18:53:30 +0700

    [UPDATE] Use student service api url from application.properties

--- Enroll Service/src/main/java/sit/electivecourseregistration/Enroll/Service/EnrollAdapter.java
+++ Enroll Service/src/main/java/sit/electivecourseregistration/Enroll/Service/EnrollAdapter.java
@@ -1,20 +1,22 @@
 package sit.electivecourseregistration.Enroll.Service;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EnrollAdapter {
+    
+    @Value("${sitecr.service.student}")
+    private String STUDENT_SERVICE_API_URL;
     public Student getStudentByStudentRecordId(Long studentRecordId) {
         try {
             RestTemplate restTemplate = new RestTemplate();
-            String url = String.format("http://localhost:8080/students/%d", studentRecordId);
+            String url = STUDENT_SERVICE_API_URL + "/students/" + studentRecordId;
 
             System.out.println(url);
 
             Student student = restTemplate.getForObject(url, Student.class);
-//            Student student = this.jsonToObjectMapper(response);;
             return student;
         } catch (Exception ex ) {
             ex.printStackTrace();
